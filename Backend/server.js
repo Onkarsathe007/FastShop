41b74dd6a2608c7c082ec71c43bdaba63975f45f
Onkar <onkarsathe96k@gmail.com>
2025-06-28 14:38:19 +0530

    chore(templating setup): ejs-mate templating setup
    -update dependancies
    - / route for home page
    - tailwind based styling to components

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const ejsMate = require("ejs-mate");
+const connectMongo = require("./conn/db.js");
 require("dotenv").config();
+
+const productModel = require("./models/productModel.js");
 const path = require("path");
+
+//connecting to mongoDB.
+connectMongo();
 
 //To handel post requrest.
 app.use(express.urlencoded({ extended: true }));
@@ -9,9 +16,16 @@ app.use(express.json());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
+//ejs mate setup.
+app.engine("ejs", ejsMate);
 
-app.get("/", (req, res) => {
-    res.render("home.ejs");
+app.get("/", async (req, res) => {
+    try {
+        var product = await productModel.find();
+        res.render("home.ejs", { product });
+    } catch (e) {
+        console.log("Error:" + e + " Occured");
+    }
 });
 
 app.listen(process.env.PORT, () => {
